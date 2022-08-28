c267570d88c76e6ea0cf3aeb9851317269083272
nikolai-in <nikolai.in@tuta.io>
2022-08-28 05:41:13 +0000

    Deploying to gh-pages from @ nikolai-in/FunCryAgar@5c36d9028af507a51a546dd478eae79301f6b00a 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1661663688|2411077";
+const CACHE_VERSION = "1661665262|2407447";
 const CACHE_PREFIX = "Fun Cry Agar-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
